ef8469322a01f2dc151434d229cb71c650d690f1
moerwald <andreas.mewald@gmail.com>
2021-08-30 15:49:28 +0200

    Finished implementation

--- src/TddInAction/TddInAction/RomanReader.Test/RomanReader.Tests.cs
+++ src/TddInAction/TddInAction/RomanReader.Test/RomanReader.Tests.cs
@@ -5,15 +5,20 @@ namespace RomanReaderTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
-        {
-            Assert.That(1.Equals(RomanReader.Parse("II")));
-        }
+        [TestCase(1, "I")]
+        [TestCase(5, "V")]
+        [TestCase(10, "X")]
+        [TestCase(50, "L")]
+        [TestCase(100, "C")]
+        [TestCase(500, "D")]
+        [TestCase(1000, "M")]
+        [TestCase(2, "II")]
+        [TestCase(20, "XX")]
+        [TestCase(4, "IV")]
+        [TestCase(9, "IX")]
+        [TestCase(14, "XIV")]
+        public void Test(int expected, string roman) =>
+            Assert.AreEqual(expected, RomanReader.Parse(roman));
     }
 }
--- src/TddInAction/TddInAction/RomanReader/RomanReader.cs
+++ src/TddInAction/TddInAction/RomanReader/RomanReader.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace At.Moerwald.RomanReader
 {
     public static class RomanReader
     {
+        private static Dictionary<char, int> _map = new Dictionary<char, int>
+        {
+            { 'I' ,    1 },
+            { 'V' ,    5 },
+            { 'X' ,   10 },
+            { 'L' ,   50 },
+            { 'C' ,  100 },
+            { 'D' ,  500 },
+            { 'M' , 1000 }
+        };
+
         public static int Parse(string roman)
         {
-            return -1;
+            int result = 0;
+
+            for (var i = 0; i< roman.Length; i++)
+            {
+                if ((i+1) < roman.Length && 
+                    (_map[roman[i]] < _map[roman[i+1]]) // Next less than actual
+                   )
+                {
+                    result += _map[roman[i + 1]] - _map[roman[i]];
+                    i++;
+                }
+                else
+                {
+                    result += _map[roman[i]];
+                }
+            }
+
+            return result;
         }
     }
 }
